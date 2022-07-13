fb1ee9a65513100194c2ccd29f5e379f0cc491eb
Wladimir Ribeiro de Souza <wladimirsouza@outlook.com>
2022-07-13 14:06:45 -0300

    Inserido verificação de data na atualização do evento.

--- LaboratorioTiaraju/LaboratorioTiaraju/ViewModel/CalendarioCQDetailViewModel.cs
+++ LaboratorioTiaraju/LaboratorioTiaraju/ViewModel/CalendarioCQDetailViewModel.cs
@@ -78,77 +78,89 @@ namespace LaboratorioTiaraju.ViewModel
 
             if (verificaConexao)
             {
-                CalendarioCQServices calendarioServices = new CalendarioCQServices();
-                string descricao = Observacao;
-                int dia = DataColeta.Day;
-                _mes = DataColeta.ToString("MMMM").ToUpper();
 
-                if (DataColeta.ToString("MMMM").ToUpper() == "JANUARY")
+                bool verificaData = DataHora.VerificaData(DataColeta);
+
+                if (verificaData)
                 {
-                    _mes = "JANEIRO";
+                    CalendarioCQServices calendarioServices = new CalendarioCQServices();
+                    string descricao = Observacao;
+                    int dia = DataColeta.Day;
+                    _mes = DataColeta.ToString("MMMM").ToUpper();
+
+                    if (DataColeta.ToString("MMMM").ToUpper() == "JANUARY")
+                    {
+                        _mes = "JANEIRO";
+                    }
+
+                    if (DataColeta.ToString("MMMM").ToUpper() == "FEBRUARY")
+                    {
+                        _mes = "FEVEREIRO";
+                    }
+
+                    if (DataColeta.ToString("MMMM").ToUpper() == "MARCH")
+                    {
+                        _mes = "MARÇO";
+                    }
+
+                    if (DataColeta.ToString("MMMM").ToUpper() == "APRIL")
+                    {
+                        _mes = "ABRIL";
+                    }
+
+                    if (DataColeta.ToString("MMMM").ToUpper() == "MAY")
+                    {
+                        _mes = "MAIO";
+                    }
+
+                    if (DataColeta.ToString("MMMM").ToUpper() == "JUNE")
+                    {
+                        _mes = "JUNHO";
+                    }
+
+                    if (DataColeta.ToString("MMMM").ToUpper() == "JULY")
+                    {
+                        _mes = "JULHO";
+                    }
+
+                    if (DataColeta.ToString("MMMM").ToUpper() == "AUGUST")
+                    {
+                        _mes = "AGOSTO";
+                    }
+
+                    if (DataColeta.ToString("MMMM").ToUpper() == "SEPTEMBER")
+                    {
+                        _mes = "SETEMBRO";
+                    }
+
+                    if (DataColeta.ToString("MMMM").ToUpper() == "OCTOBER")
+                    {
+                        _mes = "OUTUBRO";
+                    }
+
+                    if (DataColeta.ToString("MMMM").ToUpper() == "NOVEMBER")
+                    {
+                        _mes = "NOVEMBRO";
+                    }
+
+                    if (DataColeta.ToString("MMMM").ToUpper() == "DECEMBER")
+                    {
+                        _mes = "DEZEMBRO";
+                    }
+
+                    bool confirmaStatusAlterado = await calendarioServices.AtualizaDadosCalendario(dia, _mes, descricao);
+
+                    if (confirmaStatusAlterado)
+                    {
+                        await Application.Current.MainPage.DisplayAlert("Sucesso", "Evento Atualizado Com Sucesso", "OK");
+                    }
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Ops!", "A Data Informada Deve Ser Maior ou Igual a Hoje.", "OK");
                 }
 
-                if (DataColeta.ToString("MMMM").ToUpper() == "FEBRUARY")
-                {
-                    _mes = "FEVEREIRO";
-                }
-
-                if (DataColeta.ToString("MMMM").ToUpper() == "MARCH")
-                {
-                    _mes = "MARÇO";
-                }
-
-                if (DataColeta.ToString("MMMM").ToUpper() == "APRIL")
-                {
-                    _mes = "ABRIL";
-                }
-
-                if (DataColeta.ToString("MMMM").ToUpper() == "MAY")
-                {
-                    _mes = "MAIO";
-                }
-
-                if (DataColeta.ToString("MMMM").ToUpper() == "JUNE")
-                {
-                    _mes = "JUNHO";
-                }
-
-                if (DataColeta.ToString("MMMM").ToUpper() == "JULY")
-                {
-                    _mes = "JULHO";
-                }
-
-                if (DataColeta.ToString("MMMM").ToUpper() == "AUGUST")
-                {
-                    _mes = "AGOSTO";
-                }
-
-                if (DataColeta.ToString("MMMM").ToUpper() == "SEPTEMBER")
-                {
-                    _mes = "SETEMBRO";
-                }
-
-                if (DataColeta.ToString("MMMM").ToUpper() == "OCTOBER")
-                {
-                    _mes = "OUTUBRO";
-                }
-
-                if (DataColeta.ToString("MMMM").ToUpper() == "NOVEMBER")
-                {
-                    _mes = "NOVEMBRO";
-                }
-
-                if (DataColeta.ToString("MMMM").ToUpper() == "DECEMBER")
-                {
-                    _mes = "DEZEMBRO";
-                }
-
-                bool confirmaStatusAlterado = await calendarioServices.AtualizaDadosCalendario(dia, _mes, descricao);
-
-                if (confirmaStatusAlterado)
-                {
-                    await Application.Current.MainPage.DisplayAlert("Sucesso", "Evento Atualizado Com Sucesso", "OK");
-                }
+                
             }
             else
             {
